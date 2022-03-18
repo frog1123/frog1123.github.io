3a8e9ce71651772760fa7216fbf4c0482f152313
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-03-18 18:13:26 -0500

    min
    yse

--- js/playerData.js
+++ js/playerData.js
@@ -1,17 +1,1 @@
-var playerData = {
-    trianglesAmount: new Decimal("10"),
-    trianglesGenerator: {
-        tier1: {
-            hasUnlocked: false,
-            cost: new Decimal("20"),
-            costIncrease: new Decimal("1.05"),
-            lvl: 0,
-        },
-        tier2: {
-            hasUnlocked: false,
-            cost: new Decimal("100"),
-            costIncrease: new Decimal("1.07"),
-            lvl: 0,
-        }
-    }
-}
+var playerData = { trianglesAmount: new Decimal("10"), trianglesGenerator: { tier1: { hasUnlocked: false, cost: new Decimal("20"), costIncrease: new Decimal("1.05"), lvl: 0 }, tier2: { hasUnlocked: false, cost: new Decimal("100"), costIncrease: new Decimal("1.07"), lvl: 0 } } };
